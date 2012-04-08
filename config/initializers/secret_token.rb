0fd69cd8a2a4c9e034cf0cf41e69c49f36b5541d
deodatus <bohdan.shevchuk@facebook.com>
2012-04-08 08:16:16 +0300

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ordinem::Application.config.secret_token = '7b88b4d16366908528f5cefe86ebaf97592931ab77c9ab4b8bf5d7457eecbffe50f0f5c82565683fa1f5d6c8f01a7c83a4111514de2363d773e0d71a563a3620'
